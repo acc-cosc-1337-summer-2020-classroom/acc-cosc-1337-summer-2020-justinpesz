0fdbe0ff720624e553860cba41b8aff9ef8b1594
Justin Pesz <justin.pesz@g.austincc.edu>
2020-06-04 21:37:50 -0500

    Assignment Multiply Numbers

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,11 +1,24 @@
 //Write the include statement for types.h here
-
+#include "types.h"
 //Write include for capturing input from keyboard and displaying output to screen
+#include<iostream>
 
-
+using std::cout;	using std::cin;
 
 int main() 
 {
+	auto num = 0;
+	cout<<"Enter number: ";
+	cin>>num;
+
+	int result = multiply_numbers(num);
+
+	cout<<"Result is: "<<result<<"\n";
+	
+	auto  num1 = 4;
+	result = multiply_numbers(num1);
+
+	cout<<"Result is: "<<result;
 
 	return 0;
 }
--- src/examples/01_module/02_int/vars.cpp
+++ src/examples/01_module/02_int/vars.cpp
@@ -4,3 +4,9 @@ Function echo_variable returns the incoming parameter
 @param int-integer type
 @return return the int parameter
 */
+#include<vars.h>
+
+int echo_variable(int num)
+{
+    return num;
+}
