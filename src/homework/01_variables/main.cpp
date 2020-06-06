5fa4ecbe692f7f03aa62424b6563828f56ff546e
Justin Pesz <justin.pesz@g.austincc.edu>
2020-06-06 14:26:31 -0500

    Done

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -8,6 +8,7 @@ int main()
 {
 	double meal_total = 0.00;
 	double tip_percent = 0.00;
+	double total = 0.00;
 
 	cout<<"Enter meal total: $";
 	cin>>meal_total;
@@ -15,13 +16,15 @@ int main()
 	cout<<"Enter tip percentage: ";
 	cin>>tip_percent;
 	
-	double result = get_sales_tax_amount(meal_total);
-	double result2 = get_tip_amount(meal_total, tip_percent);
+	double tax_amount = get_sales_tax_amount(meal_total);
+	double tip_amount = get_tip_amount(meal_total, tip_percent);
+
+	total = meal_total + tax_amount + tip_amount;
 
 	cout<<"\nMeal Total:  $"<<std::fixed<<std::setprecision(2)<<std::setw(8)<<meal_total<<"\n";
-	cout<<"Sales Tax:   $"<<std::setw(8)<<result<<"\n";
-	cout<<"Tip applied: $"<<std::setw(8)<<result2<<"\n";
-	cout<<"Total Bill:  $"<<std::setw(8)<<meal_total + result + result2;
+	cout<<"Sales Tax:   $"<<std::setw(8)<<tax_amount<<"\n";
+	cout<<"Tip applied: $"<<std::setw(8)<<tip_amount<<"\n";
+	cout<<"Total Bill:  $"<<std::setw(8)<<total;
 
 	return 0;
 }
--- test/homework_test/01_variables_test/01_variables_tests.cpp
+++ test/homework_test/01_variables_test/01_variables_tests.cpp
@@ -6,10 +6,15 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
-TEST_CASE("Verify sum_numbers function") 
+TEST_CASE("Verify Tax amount function") 
 {
-	REQUIRE(get_sales_tax_amount(100) == 6.75);
-	REQUIRE(get_tip_amount(100, .20) == 20);
-	REQUIRE(get_tip_amount(100, .15) == 15);
+	REQUIRE(get_sales_tax_amount(10) == .675);
+	REQUIRE(get_sales_tax_amount(20) == 1.35);
+}
+
+TEST_CASE("Verify Tip Amount function")
+{	
+	REQUIRE(get_tip_amount(20, 15) == 3);
+	REQUIRE(get_tip_amount(20, 20) == 4);
 }
 
