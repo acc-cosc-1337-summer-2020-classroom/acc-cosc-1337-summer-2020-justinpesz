4bfb6c816b0c10336029b9308296321bbebe7745
Justin Pesz <justin.pesz@g.austincc.edu>
2020-06-12 22:00:40 -0500

    chc

--- src/examples/03_module/03_switch/switch.cpp
+++ src/examples/03_module/03_switch/switch.cpp
@@ -9,3 +9,25 @@
 //returns 'Option 4' when value 4
 //return 'Invalid Option' otherwise
 
+std::string(int menu)
+{
+    switch(num)
+    {
+        case 1:
+            return "Option 1";
+            break;
+        case 2:
+            return "Option 2";
+            break;
+        case 3:
+            return "Option 3";
+            break;
+        case 4:
+            return "Option 4";
+            break;
+        default:
+            return "Invalid Option"
+    }
+
+    return result;
+}
