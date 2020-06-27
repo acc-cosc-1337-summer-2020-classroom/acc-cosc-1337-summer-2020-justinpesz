44adcb5fb1a307c6b5f59b4235fe83a7eb363002
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-06-26 19:52:44 -0500

    hw 4 grading: test cases incomplete

--- test/homework_test/04_iteration_test/04_iteration_tests.cpp
+++ test/homework_test/04_iteration_test/04_iteration_tests.cpp
@@ -15,3 +15,15 @@ TEST_CASE("Verify get_gc_content")
 	REQUIRE(get_gc_content("AGCTATAG") == .375);
 	REQUIRE(get_gc_content("CGCTATAG") == .50);
 }
+
+TEST_CASE("Test Reverse string")
+{
+	REQUIRE(get_reverse_string("AGCTATAG") == "GATATCGA");
+	REQUIRE(get_reverse_string("CGCTATAG") == "GATATCGC");
+}
+
+TEST_CASE("Test DNA complement")
+{
+	REQUIRE(get_dna_complement("AAAACCCGGT") == "ACCGGGTTTT");
+	REQUIRE(get_dna_complement("CCCGGAAAAT") == "ATTTTCCGGG");
+}
